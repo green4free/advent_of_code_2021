70d5a9374639a434af85f80289ddb4113fda64f4
Magnus Östgren <ninjalostinsnow@gmail.com>
2021-12-02 17:15:00 +0100

    Solved part 1 of day 2

---
+++ day2/src/main.rs
@@ -0,0 +1,55 @@
+//rust 1.30.0 
+
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, ErrorKind, Read};
+use std::ops::Add;
+
+struct Position {
+    horizontal: i32,
+    depth: i32,
+}
+
+impl Add<Position> for Position {
+    type Output = Position;
+
+    fn add(self, _rhs: Position) -> Position  {
+        Position {horizontal: self.horizontal + _rhs.horizontal, depth: self.depth + _rhs.depth}
+    }
+}
+
+fn accumulate (sum: Result<Position, Error>, line: Result<String, Error>) -> Result<Position, Error> {
+    match sum {
+        Err(e) => Err(e),
+        Ok(s) => match line {
+            Err(e) => Err(e),
+            Ok(l) => {
+                let line_parts:Vec<&str> = l.split(" ").collect();
+                match line_parts[1].parse::<i32>() {
+                    Err(e) => Err(Error::new(ErrorKind::InvalidData, e)),
+                    Ok(v) => match line_parts[0] {
+                        "forward" => Ok(s + Position {horizontal: v, depth: 0} ),
+                        "up" => Ok(s + Position {horizontal: 0, depth: -v} ),
+                        "down" => Ok(s + Position {horizontal: 0, depth: v} ),
+                        _ => Err(Error::new(ErrorKind::InvalidData, "Non supported direction."))
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn read_and_sum<R: Read>(io: R) -> Result<Position, Error> {
+    const ZERO_POSITION: Position = Position {horizontal: 0, depth: 0};
+    let br = BufReader::new(io);
+    br.lines().fold(Ok(ZERO_POSITION), accumulate)
+}
+
+
+
+fn main() -> Result<(), Error> {
+    match read_and_sum(File::open("input.txt")?) {
+        Ok(sum) => println!("{}",sum.horizontal * sum.depth),
+        Err(error) => println!("Error: {:?}", error)
+    }
+    Ok(())
+}
