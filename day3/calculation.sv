0f5ff6bf9e4d087f755a3c205731f4c4a1de7718
Magnus Östgren <ninjalostinsnow@gmail.com>
2021-12-04 21:22:02 +0100

    Modyfied the implementation of day 3 part 1, to make changes for part 2 easier.

--- day3/calculation.sv
+++ day3/calculation.sv
@@ -3,16 +3,54 @@
 
 module calculation #(
     parameter WIDTH = 5,
-    parameter MAX_DEPTH = 1000
+    parameter DEPTH = 1000
 )(
-    input wire                clk,
-    input wire                rstn,
-    input wire [WIDTH-1:0]    data,
-    input wire                valid,
+    input wire clk,
+    input wire rstn,
+
+    input wire [WIDTH-1:0]         data,
+    input wire                     valid,
+    output reg [$clog2(DEPTH)-1:0] addr,
+    output reg                     re,
+
+    output reg                done,
     output wire [2*WIDTH-1:0] result
 );
+
+    initial begin
+        addr = 0;
+        re = 1'b0;
+        done = 1'b0;
+    end
+
+    reg init = 1'b1;
+
+    wire [$clog2(DEPTH)-1:0] next_addr = addr + 1;
+
+    always @(posedge clk, negedge rstn)
+        if (!rstn) begin
+            addr <= 0;
+            re <= 1'b0;
+            done <= 1'b0;
+            init <= 1'b1;
+        end else begin
+            init <= 1'b0;
+            re <= 1'b0;
+            if (init) begin
+                addr <= 0;
+                re <= 1'b1;
+            end else if (valid) begin
+                addr <= next_addr;
+                if (next_addr < DEPTH)
+                    re <= 1'b1;
+                else
+                    done <= 1'b1;
+            end
+
+        end
+
     wire [2*WIDTH-1:0] mostCommonParity;
-    reg signed [$clog2(MAX_DEPTH)-1:0] parityCounter [WIDTH-1:0];
+    reg signed [$clog2(DEPTH)-1:0] parityCounter [WIDTH-1:0];
 
     genvar i;
     for (i = 0; i < WIDTH; i = i + 1) begin
--- day3/tb.sv
+++ day3/tb.sv
@@ -5,35 +5,55 @@ module tb();
 
 
     reg clk = 0;
+    reg rstn = 0;
 
     always #10 clk = ~clk;
 
     reg [11:0] data = 0;
-    wire [23:0] result;
     reg valid = 0;
-    reg rstn = 0;
+    wire re;
+    wire [9:0] addr;
 
-    calculation #(.WIDTH(12)) DUT (
+    wire done;
+    wire [23:0] result;
+
+    reg [11:0] memory [1023:0];
+    
+    calculation #(.WIDTH(12), .DEPTH(1000)) DUT (
         .clk(clk),
         .rstn(rstn),
         .data(data),
         .valid(valid),
+        .addr(addr),
+        .re(re),
+        .done(done),
         .result(result)
     );
 
-    integer f;
-    initial begin
-        f = $fopen("input.txt","r");
-        rstn = 1;
-        @(posedge clk);
-        while (! $feof(f)) begin
-            $fscanf(f,"%b\n",data);
-            valid = 1;
-            @(posedge clk);
+    always @(posedge clk) begin
+        valid <= 1'b0;
+        if(re) begin
+            data <= memory[addr];
+            valid <= 1'b1;
         end
-        valid = 0;
-        #200;
+    end
+
+
+    integer f, i;
+    initial begin
+        rstn = 0;
+        f = $fopen("input.txt","r");
+        i = 0;
+        while (! $feof(f)) begin
+            $fscanf(f,"%b\n",memory[i]);
+            i = i + 1;
+        end
         $fclose(f);
+        #40;
+        rstn = 1;
+        wait(done);
+        $display("%d", result);
+        #40;
         $stop;
     end
 
